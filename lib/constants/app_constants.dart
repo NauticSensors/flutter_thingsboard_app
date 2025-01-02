b0091b37d5d7af058f3144c1c1903b8b4d3fb2fa
Simon Rijk <s.p.rijk@gmail.com>
2025-01-02 23:32:06 +0100

    fix: typo in oauth2 callback scheme

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,6 +1,6 @@
 abstract class ThingsboardAppConstants {
   static const thingsBoardApiEndpoint = 'https://bluestar.nauticsensors.com';
-  static const thingsboardOAuth2CallbackUrlScheme = 'com.nauticsensors.com.bluestar.account';
+  static const thingsboardOAuth2CallbackUrlScheme = 'com.nauticsensors.bluestar.account';
 
   static const thingsboardIOSAppSecret = 'QmtzUUhlbGFXMHlLeUNVbzFPclJIdEtPUFBKUU9kN1FnYk4zRGRsSjhTNHl5b3dOQjg3NXZIZ2d0WHpCRnhubQ==';
   static const thingsboardAndroidAppSecret = 'QmtzUUhlbGFXMHlLeUNVbzFPclJIdEtPUFBKUU9kN1FnYk4zRGRsSjhTNHl5b3dOQjg3NXZIZ2d0WHpCRnhubQ==';
