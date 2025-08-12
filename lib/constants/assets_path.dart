0d402ef9fa85fef02022ffdbe85535103b57d71f
Simon Rijk <s.p.rijk@gmail.com>
2025-08-12 12:57:45 +0200

    app name, icons, build, and translations

--- lib/constants/assets_path.dart
+++ lib/constants/assets_path.dart
@@ -1,14 +1,13 @@
 abstract class ThingsboardImage {
-  static const thingsBoardWithTitle =
-      'assets/images/thingsboard_with_title.svg';
-  static const thingsboard = 'assets/images/thingsboard.svg';
+  static const thingsBoardWithTitle = 'assets/images/bluestar_with_title.svg';
+  static const thingsboard = 'assets/images/bluestar.svg';
   static const thingsboardOuter = 'assets/images/thingsboard_outer.svg';
   static const thingsboardCenter = 'assets/images/thingsboard_center.svg';
   static const dashboardPlaceholder = 'assets/images/dashboard-placeholder.svg';
   static const deviceProfilePlaceholder =
       'assets/images/device-profile-placeholder.svg';
   static const noDataImage = 'assets/images/no-data.svg';
-  static const thingsboardBigLogo = 'assets/images/thingsboard_big_logo.svg';
+  static const thingsboardBigLogo = 'assets/images/bluestar_big_logo.svg';
   static const deviceProvisioning = 'assets/images/provisioning.svg';
   static const deviceProvisioningDone = 'assets/images/provisioning-done.svg';
   static const deviceProvisioningError =
