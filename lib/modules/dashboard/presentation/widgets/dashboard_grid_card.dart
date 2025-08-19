5f829b4184bd743795f91ebdb2a51caa1b8e39d8
Simon Rijk <s.p.rijk@gmail.com>
2025-08-19 10:38:38 +0200

    dashboard grid card

--- lib/modules/dashboard/presentation/widgets/dashboard_grid_card.dart
+++ lib/modules/dashboard/presentation/widgets/dashboard_grid_card.dart
@@ -7,7 +7,6 @@ import 'package:thingsboard_app/thingsboard_client.dart';
 import 'package:thingsboard_app/utils/utils.dart';
 
 class DashboardGridCard extends TbContextWidget {
-
   DashboardGridCard(super.tbContext, {super.key, required this.dashboard});
   final DashboardInfo dashboard;
 
@@ -28,8 +27,7 @@ class _DashboardGridCardState extends TbContextState<DashboardGridCard> {
     final hasImage = widget.dashboard.image != null;
     Widget image;
     if (hasImage) {
-      image =
-          Utils.imageFromTbImage(context, tbClient, widget.dashboard.image);
+      image = Utils.imageFromTbImage(context, tbClient, widget.dashboard.image);
     } else {
       image = SvgPicture.asset(
         ThingsboardImage.dashboardPlaceholder,
@@ -49,11 +47,7 @@ class _DashboardGridCardState extends TbContextState<DashboardGridCard> {
             child: Stack(
               children: [
                 SizedBox.expand(
-                  child: FittedBox(
-                    clipBehavior: Clip.hardEdge,
-                    fit: BoxFit.cover,
-                    child: image,
-                  ),
+                  child: FittedBox(clipBehavior: Clip.hardEdge, child: image),
                 ),
               ],
             ),
